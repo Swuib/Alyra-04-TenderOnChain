6e6539550ad7a81c08fe64a2fe43e2060c4312f3
Swuib <sebastienjdd@gmail.com>
2022-12-04 08:49:14 +0100

    update contract

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -66,9 +66,10 @@ module.exports = {
       version: "0.8.17",      // Fetch exact version from solc-bin (default: truffle's version)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
        optimizer: {
-         enabled: false,
+         enabled: true,
          runs: 200
-       }
+       },
+       viaIR : true,
       }
     }
   },
